3fc74b42ef44f7c5fd101b47bac7a23ea84139b8
Lyutink <krivenko.lyuda@gmail.com>
2021-10-13 22:36:38 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,4 @@
+const itemCategoriesRef = document.querySelectorAll('.item');
+console.log("Number of categories:", itemCategoriesRef.length);
+
+const nameCategoryRef = document.querySelector('h2');
