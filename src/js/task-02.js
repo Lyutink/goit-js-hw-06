86d66e071aebb8a0307d9d4496b3667c57b05b2e
Lyutink <krivenko.lyuda@gmail.com>
2021-10-14 16:12:39 +0300

    Update task-02.js
    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredientsRef = document.querySelector('#ingredients');
+const arrayIngredientsForAppend = [];
+
+ingredients.forEach(ingredient => {
+  const ingredientRef = document.createElement('li');
+  ingredientRef.textContent = ingredient;
+  ingredientRef.classList.add('item');
+  arrayIngredientsForAppend.push(ingredientRef);
+console.log(ingredientRef);
+});
+
+ulIngredientsRef.append(...arrayIngredientsForAppend);
