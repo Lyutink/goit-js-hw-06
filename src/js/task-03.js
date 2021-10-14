649cfdb11c7b23d1456730004f469f317f4a1795
Lyutink <krivenko.lyuda@gmail.com>
2021-10-14 17:38:17 +0300

    start task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,26 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Напиши скрипт для создания галереи изображений по массиву данных. 
+//В HTML есть список ul.gallery.
+
+// <ul class="gallery"></ul>
+// Используй массив объектов images для создания элементов <img> вложенных в <li>.
+//Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
+const ulGalleryRef = document.querySelector('.gallery');
+console.log(ulGalleryRef);
+
+const makeNewImages = images.map((image) =>
+  `<li><img class ="gallery_img" url=${image.url}, alt=${image.alt}></li>`).join("");
+
+ulGalleryRef.insertAdjacentHTML("beforeend", makeNewImages);
+ 
+const test = document.createElement('img');
+test.alt = 'White and Black Long Fur Cat';
+test.url = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+ulGalleryRef.append(test);
