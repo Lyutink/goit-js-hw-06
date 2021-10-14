985584f5707cceb48c1f4402ff7de4fa06cb6fbb
Lyutink <krivenko.lyuda@gmail.com>
2021-10-14 21:32:39 +0300

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,42 +1,22 @@
 const images = [
   {
-    //url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    //url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    //url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
-
-// Напиши скрипт для создания галереи изображений по массиву данных. 
-//В HTML есть список ul.gallery.
-
-// <ul class="gallery"></ul>
-// Используй массив объектов images для создания элементов <img> вложенных в <li>.
-//Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
-
-// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
-// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
 
 const ulGalleryRef = document.querySelector('.gallery');
 console.log(ulGalleryRef);
 
 const makeNewImages = images.map((image) =>
-  `<li class="gallery_item"><img class ="gallery_img",  src="${image.url}", alt="${image.alt}"></li>`).join("");
+  `<li class="gallery_item"><img class ="gallery_img" src="${image.url}", alt="${image.alt}"></li>`).join("");
 
 ulGalleryRef.insertAdjacentHTML("beforeend", makeNewImages);
- 
-const test = document.createElement('img');
-test.alt = 'White and Black Long Fur Cat';
-//test.url = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-test.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260';
-
-//ulGalleryRef.append(test);
