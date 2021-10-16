a0c7e666199d5b6988b6477237dd497a96a75860
Lyutink <krivenko.lyuda@gmail.com>
2021-10-16 21:55:09 +0300

    Update task-08.js
    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
+const formLoginRef = document.querySelector('.login-form');
+
+formLoginRef.addEventListener('submit', onSubmit);
+
+function onSubmit (event) {
+    event.preventDefault();
+
+    if (event.currentTarget.elements.email.value && event.currentTarget.elements.password.value === ""){
+        alert('Предупреждение!!! Все поля должны быть заполнены!');
+    } else {
+        saveData(event);
+    }
+};
+
+function saveData(event) {
+    const name = event.currentTarget.elements.email.name;
+    const password = event.currentTarget.elements.password.name;
+
+    const dataInForm = {
+        [name]:  event.currentTarget.elements.email.value,
+        [password]: event.currentTarget.elements.password.value,
+    }
+
+    console.log(dataInForm);
+    event.currentTarget.reset();
+}
+
 
