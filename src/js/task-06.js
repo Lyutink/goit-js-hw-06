1f01d0ee8c3f7346833a24175bbcc2c14a684883
Lyutink <krivenko.lyuda@gmail.com>
2021-10-16 18:06:41 +0300

    Update task-06.js
    udate name function

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,9 +2,9 @@ const validationInputRef = document.querySelector('#validation-input');
 
 const lengthInputCorrect = Number(validationInputRef.dataset.length);
 
-validationInputRef.addEventListener('blur', foo);
+validationInputRef.addEventListener('blur', onValidationOutOfFocus);
 
-function foo() {
+function onValidationOutOfFocus() {
     validationInputRef.classList.add('invalid');
     if (validationInputRef.value.length === lengthInputCorrect) {
         validationInputRef.classList.replace('invalid', 'valid');
