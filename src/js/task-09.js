9be3cf8984f9452984b8c167679fba1dd4427241
Lyutink <krivenko.lyuda@gmail.com>
2021-10-16 22:20:43 +0300

    Update task-09.Js
    Update task-09.Js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const btnChangeColorRef = document.querySelector('.change-color');
+const spancolorRef = document.querySelector('.color');
+
+btnChangeColorRef.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  console.log(newColor);
+  document.body.style.background = `${newColor}`;
+  spancolorRef.textContent = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
